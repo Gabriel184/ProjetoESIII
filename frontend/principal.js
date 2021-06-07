674d81f5dc18d1608c58e6eec876aba62c0e4520
Gabriel <gabriel.oliveira184@fatec.sp.gov.br>
2021-06-06 23:31:30 -0300

    Modais adicionados e pre preenchidos com dados

--- frontend/principal.js
+++ frontend/principal.js
@@ -1,18 +1,40 @@
 var table = document.querySelector('table#tabela'); // recebe tabela que vai receber daodos
 
-var lista = [{"descricao":"bisicreta","valor":60,"codigo":"123","id":2},
-    {"descricao":"homocinética","valor":500,"codigo":"456","id":3},
-    {"descricao":"baixo","valor":2000,"codigo":"789","id":4},
-    {"descricao":"notebook lenovo","valor":500,"codigo":"000","id":5},
-    {"descricao":"macaco","valor":321,"codigo":"4002","id":6},
-    {"descricao":"Viola Caipira","valor":400.34,"codigo":"1010","id":7},
-    {"descricao":"Diminuidor de cabeça","valor":400,"codigo":"666","id":8},
-    {"descricao":"produto foda","valor":69,"codigo":"6969","id":9},
-    {"descricao":"Xavleg","valor":157,"codigo":"171","id":11}];
+var lista = [{"nomeAluno":"bisicreta","Curso":60,"matricula":"123","idAluno":2},
+    {"nomeAluno":"homocinética","Curso":500,"matricula":"456","idAluno":3},
+    {"nomeAluno":"baixo","Curso":2000,"matricula":"789","idAluno":4},
+    {"nomeAluno":"notebook lenovo","Curso":500,"matricula":"000","idAluno":5},
+    {"nomeAluno":"macaco","Curso":321,"matricula":"4002","idAluno":6},
+    {"nomeAluno":"Viola Caipira","Curso":400.34,"matricula":"1010","idAluno":7},
+    {"nomeAluno":"DiminuidAlunoor de cabeça","Curso":400,"matricula":"666","idAluno":8},
+    {"nomeAluno":"produto foda","Curso":69,"matricula":"6969","idAluno":9},
+    {"nomeAluno":"Xavleg","Curso":157,"matricula":"171","idAluno":11}];
 
-    printTable(table,lista);
+window.onload = function(){
 
-function printTable(table,lista) {
+    var updateModal = function (msg){
+
+        alert(msg);
+        
+    }
+
+    var deleteAluno = function(id){
+
+        confirm("Você deseja mesmo deletar esse aluno");
+
+    }
+
+    var readModal = function(matricula){
+
+        criarModal();
+
+    }
+
+    printTable(table,lista,updateModal,deleteAluno,readModal);    
+
+}
+
+function printTable(table,lista,updateModal,deleteAluno,readModal) {
 
     //Abaixo criação de cabeçalho
     let thead = document.createElement('thead');
@@ -37,18 +59,65 @@ function printTable(table,lista) {
         for(let i=0;i<Object.keys(lista[0]).length;i++) {
             data[i] = document.createElement('td');
         }
-        data[0].innerText = `${lista[i].id}`;
+        data[0].innerText = `${lista[i].idAluno}`;
         row.appendChild(data[0]);
-        data[1].innerText = `${lista[i].codigo}`;
+        data[1].innerText = `${lista[i].matricula}`;
         row.appendChild(data[1]);
-        data[2].innerText = `${lista[i].descricao}`;
+        data[2].innerText = `${lista[i].nomeAluno}`;
         row.appendChild(data[2]);
-        data[3].innerText = `${lista[i].valor}`;
+        data[3].innerText = `${lista[i].Curso}`;
         row.appendChild(data[3]);
+
+        var updateIcon = document.createElement('img');
+        updateIcon.setAttribute("src", "../images/update.png");
+        //updateIcon.addEventListener("click",updateModal(lista[i].nomeAluno));
+
+        var zoomIcon = document.createElement('img');
+        zoomIcon.setAttribute("src", "../images/zoom.png");
+
+        var deleteIcon  = document.createElement('img');;
+        deleteIcon.setAttribute("src", "../images/lixeira.png");
+
+        var update = document.createElement('td');
+        update.setAttribute("class","updateButtton");
+        update.addEventListener("click",function(){
+
+            updateModal(lista[i].nomeAluno);
+            
+        });
+        update.appendChild(updateIcon);
+        row.appendChild(update);
+        
+        var readAll = document.createElement('td');
+        readAll.setAttribute("class","getAllButtton");
+        readAll.addEventListener("click",function(){
+
+            readModal();
+
+        });
+        readAll.appendChild(zoomIcon);
+        row.appendChild(readAll);
+
+        var _delete = document.createElement('td');
+        _delete.setAttribute("class","_deleteButtton");
+        _delete.addEventListener("click",function(){
+
+            deleteAluno(lista[i].idAluno);
+
+        });
+        _delete.appendChild(deleteIcon);
+        row.appendChild(_delete);
+
         tbody.appendChild(row);
         
     }
 
     table.appendChild(tbody);
+
+}
+
+function criarModal(){
+
+    console.log("FUUNFO");
 
 }
